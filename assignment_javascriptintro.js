a4a4c659680b937118ad97716bef9e2792abb710
GAGAN DEEP SINGH <108108276+skylamplion@users.noreply.github.com>
2022-09-15 23:25:29 +0530

    Add files via upload

---
+++ assignment_javascriptintro.js
@@ -0,0 +1,8 @@
+// accessing the second paragraph and adding text to the paragraph
+document.getElementById("head").innerHTML = "Contact Us";
+// accessing the heading and adding text color to it
+document.getElementById('max').style.color='peach';
+// accessing the first paragraph and modifying the font size of it
+document.getElementById('max').style.fontSize='100px';
+// accessing the whole body elements of the HTML and adding thebackground color to it
+document.body.style.backgroundColor  = "rgb(87, 196, 165);";
